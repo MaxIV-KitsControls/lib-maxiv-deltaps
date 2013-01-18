2d571a0f62ee1d37e8c4c9ab01c06263aa59c6ce
Vincent Hardion <vincent.hardion@maxlab.lu.se>
2013-01-18 17:03:12 +0100

    Refactoring the test in function and some common code Add test for : - Software Trigger (!!! with set_trigger_delay()) - Hardware Trigger (not tested yet) - Communication : nb socket, time to request

--- test/TestClass.cpp
+++ test/TestClass.cpp
@@ -2,6 +2,10 @@
 #include <iostream>
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/time.h>
+#include <math.h>
 
 #include <vector>
 #include <sys/types.h>
@@ -19,15 +23,25 @@
 using namespace std;
 using namespace itest;
 itPole2811 *Magnet=0;
+struct idata myDataX_Magnet1;
 
-main (int argc, char **argv)
-{ 
-	int val=0;
-	struct idata myDataX_Magnet1;
 
-/*	try{
+/* Return 1 if the difference is negative, otherwise 0.  */
+long int timeval_subtract(struct timeval *result, struct timeval *t2, struct timeval *t1)
+{
+    long int diff = (t2->tv_usec + 1000000 * t2->tv_sec) - (t1->tv_usec + 1000000 * t1->tv_sec);
+//    result->tv_sec = diff / 1000000;
+//    result->tv_usec = diff % 1000000;
+
+    return diff;
+}
+
+void test0()
+{
+
+	try{
 		std::cout<<"---------------TEST0---------------"<<std::endl;
-		Magnet = new itPole2811("192.168.150.101",1,4);
+		Magnet = new itPole2811("192.168.150.101",1,2);
 		std::cout<<Magnet->addrIP()<<std::endl;
 		float tab[]={1.,2.,2.001,1.,-0.5,-3};
 		Magnet->set_current_buffer(tab,0,6);
@@ -48,6 +62,8 @@ main (int argc, char **argv)
 		Magnet->set_current_buffer_loop(1);
 		Magnet->set_current_latch_source(CURR_LATCH_SOURCE_VALUE);
 		Magnet->set_state(MAGNET_OFF);
+
+		delete Magnet;
 	}
 	catch(ItestException &e)
 	{
@@ -58,9 +74,13 @@ main (int argc, char **argv)
 		}
 		delete Magnet;
 	}
+}
+void test1()
+{
+
 	try{
 		std::cout<<"---------------TEST1---------------"<<std::endl;
-		Magnet = new itPole2811("192.168.150.101",1,4);
+		Magnet = new itPole2811("192.168.150.101",1,2);
 		std::cout<<Magnet->addrIP()<<std::endl;
 		Magnet->set_current(1);
 		std::cout<<"CURRENT SETTING:"<<1<<std::endl;
@@ -96,11 +116,14 @@ main (int argc, char **argv)
 		}
 		delete Magnet;
 	}	
-*/
-/*
+}
+
+bool test2()
+{
+	bool result=false;
 	try{
 		std::cout<<"---------------TEST2---------------"<<std::endl;
-		Magnet = new itPole2811("192.168.150.101",1,4);
+		Magnet = new itPole2811("192.168.150.101",1,2);
 		Magnet->clear_all_err();
 		Magnet->set_trigger_state(TRIGGER_STATE_ON);
 		Magnet->set_state(MAGNET_ON);
@@ -108,6 +131,7 @@ main (int argc, char **argv)
 		Magnet->set_current(1.12345);
 		std::cout<<"CURRENT SETTING:"<<1.12345<<std::endl;
 		Magnet->set_current_tracking_level(50e-6);	
+		sleep(2);
 		//-------------------------------------------------------------------	
 		do{
 			std::cout<<"\r"<<"MEASCURR1 :"<<Magnet->get_measure_current();
@@ -134,6 +158,7 @@ main (int argc, char **argv)
 		std::cout<<"CURRENT TRIGGER SETTING:"<<-2.65432<<std::endl;
 		Magnet->set_trigger_delay(3);
 
+		sleep(10);
 		initiate_udp_trigger("192.168.150.255");
 		//-------------------------------------------------------------------
 		do{
@@ -201,10 +226,15 @@ main (int argc, char **argv)
 		}
 		delete Magnet;
 	}	
+	return result;
+}
 
+bool test3()
+{
+	bool result=false;
 	try{
-		std::cout<<"---------------TEST2---------------"<<std::endl;
-		Magnet = new itPole2811("192.168.150.101",1,4);
+		std::cout<<"---------------TEST3---------------"<<std::endl;
+		Magnet = new itPole2811("192.168.150.101",1,2);
 		Magnet->clear_all_err();
 		Magnet->set_trigger_state(TRIGGER_STATE_ON);
 		Magnet->set_state(MAGNET_ON);
@@ -278,11 +308,18 @@ main (int argc, char **argv)
 					e.errors[i].code<<std::endl;
 		}
 		delete Magnet;
-	}*/
+	}
+
+	return result;
+}
+
+bool test4()
+{
+	bool result=false;
 
 	try{
-		std::cout<<"---------------TEST0---------------"<<std::endl;
-		Magnet = new itPole2811("192.168.150.101",3,2);
+		std::cout<<"---------------TEST4---------------"<<std::endl;
+		Magnet = new itPole2811("192.168.150.101",1,2);
 		std::cout<<Magnet->addrIP()<<std::endl;
 
 		//-------------------------------------------------------------------
@@ -331,6 +368,7 @@ main (int argc, char **argv)
 		std::cout<<"MEASCURR :"<<Magnet->get_measure_current()<<std::endl;
 		std::cout<<"-----------------------------------"<<std::endl;
 		
+		delete Magnet;
 	}
 	catch(ItestException &e)
 	{
@@ -341,6 +379,308 @@ main (int argc, char **argv)
 		}
 		delete Magnet;
 	}
-    return 0;
+
+	return result;
+}
+
+void init_pole(itPole2811* pole)
+{
+	pole->clear_all_err();
+	pole->set_trigger_state(TRIGGER_STATE_ON);
+	pole->set_state(MAGNET_ON);
+	pole->set_current(0);
+
+	
+	pole->set_current_tracking_level(50e-6);	
+	//-------------------------------------------------------------------	
+	do{
+		std::cout<<"\r"<<"MEASCURR1 :"<<pole->get_measure_current();
+	}while(!pole->get_current_tracking_status());
+	std::cout<<std::endl;
+}
+
+void release_pole(itPole2811* pole)
+{
+	sleep(2);
+	pole->set_state(MAGNET_OFF);
+}
+
+void display_counter(itPole2811* pole)
+{
+	int ctn1,ctn2;
+	pole->get_ctn(&ctn1,&ctn2);
+	std::cout<<"---------------CTN---------------"<<std::endl;
+	std::cout<<"CTN3 :"<<ctn1<<std::endl;
+	std::cout<<"CTN4 :"<<ctn2<<std::endl;
+}
+
+void display_pole(itPole2811* pole)
+{
+
+	pole->get_idata(&myDataX_Magnet1);
+
+	std::cout<<"---------------IDATA---------------"<<std::endl;
+	std::cout<<"MAGNET :"<<myDataX_Magnet1.magnet<<std::endl;
+	std::cout<<"POLE :"<<myDataX_Magnet1.pole<<std::endl;
+	std::cout<<"STATE :"<<myDataX_Magnet1.state<<std::endl;
+	std::cout<<"FAIL :"<<myDataX_Magnet1.fail<<std::endl;
+	std::cout<<"MEASVOLT :"<<myDataX_Magnet1.measvolt<<std::endl;
+	std::cout<<"MEASCURR :"<<myDataX_Magnet1.meascurr<<std::endl;
+	std::cout<<"MEASCURR :"<<pole->get_measure_current()<<std::endl;
+	std::cout<<"-----------------------------------"<<std::endl;
+}
+
+void wait_slope_completion(itPole2811* pole)
+{
+	std::cout<<"WAIT CURRENT SLOPE COMPLETION "<<std::endl;
+	do{
+		std::cout << "\r" << "Current :" << pole->get_measure_current();
+	}while( !pole->get_current_slope_completion() );
+	std::cout<<std::endl;
+
+}
+void wait_tracking(itPole2811* pole)
+{
+	std::cout<<"WAIT CURRENT TRACKING COMPLETION "<<std::endl;
+	do{
+		std::cout << "\r" << "Current :" << pole->get_measure_current();
+	}while( !pole->get_current_tracking_status() );
+	std::cout<<std::endl;
+}
+
+bool test_trigger(bool software_trigger)
+{
+	bool result=false;
+	itPole2811* pole1=0;
+	itPole2811* pole2=0;
+
+	double expected_current1=0.2;
+	double expected_current2=0.2;
+
+	try{
+		std::cout<<"---------------TEST TRIGGER---------------"<<std::endl;
+		// Initialise the different pole to synchronise
+		std::cout<<"INITIALISE POLE 1"<<std::endl;
+		pole1 = new itPole2811("192.168.150.101",1,1);
+		init_pole(pole1);
+		display_pole(pole1);
+
+		std::cout<<"INITIALISE POLE 2"<<std::endl;
+		pole2 = new itPole2811("192.168.150.101",1,5);
+		init_pole(pole2);
+		display_pole(pole2);
+			
+		//Set the current latch to trig
+		std::cout<<">CURRENT TRIGGER SETTING"<<std::endl;
+		std::cout<<"POLE 1 : "<< expected_current1 <<std::endl;
+		pole1->set_current_latch(expected_current1);
+		pole1->set_trigger_delay(3);
+		pole1->set_trigger_state(true);
+		std::cout<<"POLE 2 : "<< expected_current2 <<std::endl;
+		pole2->set_current_latch(expected_current2);
+		pole2->set_trigger_delay(3);
+		pole2->set_trigger_state(true);
+
+		if(software_trigger)
+		{
+			std::cout<<"SOFTWARE TRIGGER : "<< "192.168.150.255" <<std::endl;
+			initiate_udp_trigger("192.168.150.255");
+		}
+		else
+		{
+			std::cout<<"HARDWARE TRIGGER : WAITING FOR EVENT" <<std::endl;
+			sleep(5);
+		}
+
+		//-------------------------------------------------------------------
+		//Display current current value of both poles
+		//should be threaded to follow up in live 
+		//but the purpose is to test the triggers
+		wait_slope_completion(pole1);
+		wait_tracking(pole1);
+
+		wait_slope_completion(pole2);
+		wait_tracking(pole2);
+
+		display_pole(pole1);
+		display_pole(pole2);
+		//-------------------------------------------------------------------
+		//release_pole(pole1);
+		//release_pole(pole2);
+		
+		display_counter(pole1);
+		display_counter(pole2);
+
+		delete pole1;
+		delete pole2;
+	}
+	catch(ItestException &e)
+	{
+		for(int i=0;i<e.errors.size();i++)		//gestion des exceptions empilées dans le vecteur d'erreur
+       		{
+			std::cout<<e.errors[i].reason<<" , "<<e.errors[i].desc<<" , "<<e.errors[i].origin<<" , "<<
+					e.errors[i].code<<std::endl;
+		}
+		delete pole1;
+		delete pole2;
+	}	
+	return result;
+}
+
+void display_current(itPole2811* pole)
+{
+	//std::cout<<std::endl;
+	try
+	{
+		std::cout << "\r" << "Current :" << pole->get_measure_current();
+		std::cout.flush(); 
+	}
+	catch(ItestException &e)
+	{
+		for(int i=0;i<e.errors.size();i++)		//gestion des exceptions empilées dans le vecteur d'erreur
+       		{
+			std::cout<<e.errors[i].reason<<" , "<<e.errors[i].desc<<" , "<<e.errors[i].origin<<" , "<<
+					e.errors[i].code<<std::endl;
+		}
+	}
+}
+
+void display_request_rate(itPole2811* pole)
+{
+	timeval start, end, ellapsed;
+	int count=0;
+	int error=0;
+	long int time=0;
+
+	while(true)
+	{
+		
+		try
+		{
+			gettimeofday(&start, NULL);
+			pole->get_measure_current();
+			gettimeofday(&end, NULL);
+		}
+		catch(ItestException &e)
+		{
+			gettimeofday(&end, NULL);
+			error++;
+			for(int i=0;i<e.errors.size();i++)		//gestion des exceptions empilées dans le vecteur d'erreur
+	       		{
+				std::cout<<e.errors[i].reason<<" , "<<e.errors[i].desc<<" , "<<e.errors[i].origin<<" , "<<
+						e.errors[i].code<<std::endl;
+			}
+		}
+		time = timeval_subtract(&ellapsed, &end, &start);
+		std::cout << "\r" << "Time to serve ("<<error<<"): "<< time<< "             ";
+		std::cout.flush(); 
+	}
+}
+
+int test_max_connection(const char *ip, int pole_number, int max)
+{
+	std::cout << "LAUNCH TEST MAX CONNECTION : "<< ip << " " << pole_number << " " << max << std::endl;
+	int result=0;
+	int counter=0;
+	int MAX=max;
+	
+	itPole2811** poles = new itPole2811*[MAX];
+
+        try
+        {
+		// try the maximum of connection
+		// To check the connection the only way is to send a request
+		// (The constructor is protected against socket Exception)	
+		for(int i=0;i<MAX;i++)
+		{
+			std::cout << "[DEBUG] CREATE CONNECTION "<< i << std::endl;
+			poles[i] = new itPole2811(ip,1,pole_number);
+			counter++;
+			poles[i]->get_measure_current();
+			result++;
+		}
+        }
+        catch(ItestException &e)
+        {
+                for(int i=0;i<e.errors.size();i++)              //gestion des exceptions empilées dans le vecteur d'erreur
+                {
+                        std::cout<<e.errors[i].reason<<" , "<<e.errors[i].desc<<" , "<<e.errors[i].origin<<" , "<<
+                                        e.errors[i].code<<std::endl;
+                }
+        }
+	
+	for(int i=0;i<counter;i++)
+	{
+		std::cout << "[DEBUG] DELETE CONNECTION "<< i << std::endl;
+		delete poles[i];
+	}
+	delete[] poles;
+
+	return result;
+}
+
+main (int argc, char **argv)
+{ 
+	int val=0;
+	if(argc>1)
+	{
+		// Display the current current of one pole
+		if(strcmp(argv[1],"display-current")==0 && argc>3)
+		{
+			std::cout << "Current display (pole="<<argv[3]<<";ip="<<argv[2]<<")"<<std::endl;
+
+			std::string ip = argv[2]; 
+			int pole_number = atoi(argv[3]);
+
+			itPole2811* pole = new itPole2811(ip.c_str(),1,pole_number);
+			// Display the value indefinitively
+			int i=0;
+			while(true)
+			{
+				//std::cout<<"Frame "<<i++<<std::endl;
+				display_current(pole);
+				sleep(1);
+			}
+			// In case of ...
+			delete pole;
+		}
+		// Display the request rate
+		else if(strcmp(argv[1],"display-request-rate")==0 && argc>3)
+		{
+			std::cout << "Current display (pole="<<argv[3]<<";ip="<<argv[2]<<")"<<std::endl;
+
+			std::string ip = argv[2]; 
+			int pole_number = atoi(argv[3]);
+
+			itPole2811* pole = new itPole2811(ip.c_str(),1,pole_number);
+			// Display the value indefinitively
+			//std::cout<<"Frame "<<i++<<std::endl;
+			display_request_rate(pole);
+			// In case of ...
+			delete pole;
+		}
+		// Launch the trigger test
+		else if (strcmp(argv[1],"test-trigger")==0 && argc>2)
+		{
+			bool software_trigger = strcmp(argv[2],"SOFTWARE")==0;
+			test_trigger(software_trigger);
+		}
+		// Launch the max of connection test
+		else if (strcmp(argv[1],"test-connection")==0 && argc>4)
+		{
+			int result = test_max_connection(argv[2],atoi(argv[3]),atoi(argv[4]));
+			std::cout << "Number max of connection acception :"<< result << std::endl;
+		}
+		else
+		{
+			while(argc--)
+			{
+                		printf("%s\n", *argv++);
+			}
+		}
+		
+			
+	}
+	return 0;
 }
 
