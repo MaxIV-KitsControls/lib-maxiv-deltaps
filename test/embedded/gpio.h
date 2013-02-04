f7b44d086a0d68fb1e89982b357818908a49bcf5
Andreas Persson <andreas_g.persson@maxlab.lu.se>
2013-02-04 16:21:53 +0100

    test/embedded/gpio.h: Whitespace fixes

--- test/embedded/gpio.h
+++ test/embedded/gpio.h
@@ -14,34 +14,34 @@ extern "C" {
  * NOTE: this function must be called prior to using
  * gpio_set() or gpio_get().
  *
- * @param pin			pin number
- * @param direction		0 : input, positive : output
- * @return 				0 on success, -1 on failure
+ * @param pin        pin number
+ * @param direction  0 : input, positive : output
+ * @return           0 on success, -1 on failure
  */
 int gpio_init(unsigned int pin, unsigned int direction);
 
 /**
  * release the specified gpio pin and free allocated resources
  *
- * @param pin			pin number
- * @return 				0 on success, -1 on failure
+ * @param pin        pin number
+ * @return           0 on success, -1 on failure
  */
 int gpio_free(unsigned int pin);
 
 /**
  * Set the specified gpio pin high or low
  *
- * @param pin			pin number
- * @param value			0 : logic '0', positive : logic '1'
- * @return 				0 on success, -1 on failure
+ * @param pin        pin number
+ * @param value      0 : logic '0', positive : logic '1'
+ * @return           0 on success, -1 on failure
  */
 int gpio_set(unsigned int pin, unsigned int value);
 
 /**
  * Read the value of the specified gpio pin
  *
- * @param pin			pin number
- * @return 				0 : logic '0', positive : 'logic 1', -1 : failure
+ * @param pin        pin number
+ * @return           0 : logic '0', positive : 'logic 1', -1 : failure
  */
 int gpio_get(unsigned int pin);
 
